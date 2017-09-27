a2b64c2d5804e5df4ccab9715b7fd26baaa1bd1d
kelly-keating <keating.kel@gmail.com>
2017-09-28 10:21:18 +1300

    initial commit

---
+++ python_intro.py
@@ -0,0 +1,23 @@
+# PYTHONNNNNNN
+print('Hello, Django girls!')
+
+if 3 > 2:
+    print('It works!')
+
+name = 'Sonja'
+if name == 'Ola':
+    print('Hey Ola!')
+elif name == 'Sonja':
+    print('Hey Sonja!')
+else:
+    print('Hey anonymous!')
+
+def hi(name):
+    print('Hi ' + name + '!')
+
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+for name in girls:
+    hi(name)
+
+for i in range(1, 6):
+    print(i)
